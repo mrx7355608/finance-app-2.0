2063f6f15c5e1e637b24305cc4aa3c3e6870051d
mrx7355608 <mrx7355608@gmail.com>
2025-03-16 07:14:41 +0500

    migrated expenses table

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -2,11 +2,15 @@
 
 import journal from './meta/_journal.json';
 import m0000 from './0000_broad_sleepwalker.sql';
+import m0001 from './0001_normal_ender_wiggin.sql';
+import m0002 from './0002_public_lady_bullseye.sql';
 
   export default {
     journal,
     migrations: {
-      m0000
+      m0000,
+m0001,
+m0002
     }
   }
   
